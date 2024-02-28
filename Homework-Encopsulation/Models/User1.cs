fe35b83dad9bb7b998f863391dcd766dc21f65d5
zeyqam <zegamda@code.edu.az>
2024-02-29 01:20:45 +0400

    classtask

---
+++ Homework-Encopsulation/Models/User1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_Encopsulation.Models
+{
+    internal class User1
+    {
+        public string name;
+        public string surname;
+        public int age;
+        private string password;
+
+
+        public User1(string name, string surname, int age, string password)
+        {
+            name = name;
+            surname = surname;
+            age = age;
+            password = password;
+        }
+
+
+        public void SetPassword(string newPassword)
+        {
+            password = newPassword;
+        }
+        public string GetPassword()
+        {
+            return password;
+        }
+    }
+}
--- Homework-Encopsulation/Program.cs
+++ Homework-Encopsulation/Program.cs
@@ -1,7 +1,13 @@
 ﻿using Homework_Encopsulation.Controllers;
+using Homework_Encopsulation.Models;
 
-UserController userController = new();
+//UserController userController = new();
 //userController.GetAll();
 //userController.GetById();
-AccountController accountController = new AccountController();
-accountController.Login();
+//AccountController accountController = new AccountController();
+//accountController.Login();
+User1 user1 = new User1("Arif", "Abiyev", 30, "password123");
+Console.WriteLine("Current Password: " + user1.GetPassword());
+user1.SetPassword("newPassword456");
+Console.WriteLine("New Password: " + user1.GetPassword());
+Console.ReadLine();
